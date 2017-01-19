6d599a73d0892d9a309eaaa9f7ce0c28f6cc15f2
Tangren_ <996489865@qq.com>
2017-01-19 20:21:47 +0800

    无限循环ViewPager

---
+++ app/src/main/java/com/viewpage/Config.java
@@ -0,0 +1,14 @@
+package com.viewpage;
+
+/**
+ * 作者：Tangren_ on 2017/1/19 16:52.
+ * 邮箱：wu_tangren@163.com
+ * TODO:一句话描述
+ */
+
+public class Config {
+    public static final String URL_1 = "http://img6.17ugo.com/images/jjbh/dsyzjkwxxcq/dsyzjkwxxcq_45.jpg";
+    public static final String URL_2 = "http://www.0516tx.com/files/2013-9/20130923124514134077.jpg";
+    public static final String URL_3 = "http://www.0516tx.com/files/2013-9/20130923124431105169.jpg";
+    public static final String URL_4 = "http://www.0516tx.com/files/2013-9/20130923124452120289.jpg";
+}
---
+++ app/src/main/java/com/viewpage/MainActivity.java
@@ -0,0 +1,181 @@
+package com.viewpage;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+
+    @InjectView(R.id.viewPager)
+    ViewPager viewPager;
+    @InjectView(R.id.title)
+    TextView title;
+    @InjectView(R.id.dot_group)
+    LinearLayout dotGroup;
+    @InjectView(R.id.activity_main)
+    RelativeLayout activityMain;
+
+    private String[] imageUrl = {"http://img6.17ugo.com/images/jjbh/dsyzjkwxxcq/dsyzjkwxxcq_45.jpg",
+            "http://www.0516tx.com/files/2013-9/20130923124514134077.jpg",
+            "http://www.0516tx.com/files/2013-9/20130923124431105169.jpg",
+            "http://www.0516tx.com/files/2013-9/20130923124452120289.jpg"};
+    private List<ImageView> imageList;
+    private LinearLayout dot_group;//小圆点
+    private String[] imageDescArrs;
+    private static final String TAG = "MainActivity";
+    private boolean isSwitchPager = false; //默认不切换
+    private int previousPosition = 0; //默认为0
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        initView();
+    }
+
+    private void initView() {
+        initViewPagerData();
+        viewPager.setAdapter(new MyViewPager());
+
+        //设置当前viewPager要显示的第几个条目
+        int item = Integer.MAX_VALUE / 2 - (Integer.MAX_VALUE / 2 % imageList.size());
+        Log.d(TAG, "item=" + item);
+        viewPager.setCurrentItem(item);
+
+        //把第一个小圆点设置成白色，显示第一个TExtView内容
+        dotGroup.getChildAt(previousPosition).setEnabled(true);
+        title.setText(imageDescArrs[previousPosition]);
+        //设置viewPager滑动监听事件
+        viewPager.addOnPageChangeListener(this);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!isSwitchPager) {
+                    SystemClock.sleep(3000);
+                    handler.sendEmptyMessage(0);
+                }
+            }
+        }).start();
+
+    }
+
+    private void initViewPagerData() {
+        imageDescArrs = new String[]{"标题1", "标题2", "标题3", "标题4", "标题5"};
+        imageList = new ArrayList<ImageView>();
+        ImageView im;
+        View dotView;
+        for (int i = 0; i < imageDescArrs.length; i++) {
+            im = new ImageView(this);
+            Glide.with(this).load(imageUrl[i])
+                    .crossFade()
+                    .centerCrop()
+                    .into(im);
+            imageList.add(im);
+            //准备小圆点数据
+            dotView = new View(getApplicationContext());
+            dotView.setBackgroundResource(R.drawable.selector_dot);
+
+            //设置小圆点宽和高
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(15, 15);
+            //设置每个小圆点之间的距离
+            if (i != 0) {
+                params.leftMargin = 15;
+            }
+            dotView.setLayoutParams(params);
+            //设置小圆点状态
+            dotView.setEnabled(false);
+            //把dotView加入到线性布局中
+            dotGroup.addView(dotView);
+        }
+
+    }
+
+    private class MyViewPager extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return Integer.MAX_VALUE;
+        }
+
+        //初始化每个条目要显示的内容
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            int newPostion = position % imageList.size();
+            //获取到条目要显示的内容imageView
+            ImageView img = imageList.get(newPostion);
+            container.addView(img);
+            return img;
+        }
+
+        //是否复用当前view
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        //销毁条目
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+
+            container.removeView((View) object);
+        }
+    }
+
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        int newPostion = position % imageList.size();
+        //取出postion的位置小圆点设置为true
+        dotGroup.getChildAt(newPostion).setEnabled(true);
+        //把一个小圆点设置为false
+        dotGroup.getChildAt(previousPosition).setEnabled(false);
+        title.setText(imageDescArrs[newPostion]);
+        previousPosition = newPostion;
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isSwitchPager = false;
+    }
+}
